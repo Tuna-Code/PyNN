544c3e5cc15baae17cec9e50be1346b634e72bd6
Josh Tunnissen <josh.tunnissen@gmail.com>
2023-01-25 08:26:41 -0800

    Started NN Side

---
+++ Net/layer.py
@@ -0,0 +1,13 @@
+from Math.objs import Mat
+
+
+class Layer:
+    def __init__(self, num_nodes, actv_func):
+        prefill = ("zeroes",0,0,-1)
+        self.input = Mat(num_nodes, 1, prefill)
+        self.output = Mat(num_nodes, 1, prefill)
+        self.actv_func = actv_func
+
+
+temp = Layer(3,"Tmp")
+print(temp.input)
---
+++ Net/net.py
@@ -0,0 +1,5 @@
+from layer import *
+
+class Net:
+    def __init__(self, num_layers, cols, prefill):
+        print("hi")
