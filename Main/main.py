da90c61cfaa96d9880832c2c558956aea8a76e58
Josh Tunnissen <josh.tunnissen@gmail.com>
2023-01-30 12:44:51 -0800

    Customkinter gui is now the way

--- Main/main.py
+++ Main/main.py
@@ -1,28 +1,30 @@
-from Math.objs import Mat
 from Net.net import Net
 import net_helper
 import os
-from GUI.gui import Gui
+
+from GUI.gui import GUI
+
+
 os.system("clear")
 
 
-gui = Gui()
+
+gui = GUI()
+gui.mainloop()
+#net_file = gui.file_select()
 
 
-net_file = gui.file_select()
-
-
-nn = net_helper.createNetFromFile(net_file)
+#nn = net_helper.createNetFromFile(net_file)
 
 
 
 
 
-nn.forward_prop()
+#nn.forward_prop()
 
 
 
 #nn.forward_prop(input, exp_out)
 
 
-print(nn)
+#print(nn)
